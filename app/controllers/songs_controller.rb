f88597c69944a28cf440e23cf93a6d81c91e5625
Jo Fischer <josephinefischer9@gmail.com>
2020-06-10 18:57:09 -0400

    flash still sucks

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -24,7 +24,21 @@ class SongsController < ApplicationController
   end
 
   get "/songs/:slug/edit" do
-    @
+    @song = Song.find_by_slug(params[:slug])
+    @genres = Genre.all
+    erb :"/songs/edit"
+  end
+
+  patch "/songs/:slug" do
+    song = Song.find_by_slug(params[:slug])
+    song.update(params[:song])
+
+    if !params[:artist][:name].empty?
+      song.artist = Artist.find_or_create_by(params[:artist])
+      song.save
+    end
+    flash[:message] = "Successfully updated song."
+    redirect to "/songs/#{song.slug}"
   end
 
 end
